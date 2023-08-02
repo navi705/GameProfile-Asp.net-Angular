db5e034bc6ad5454409dc427afb4d96dac28c65c
perevos10@yandex.ru <perevos10@yandex.ru>
2023-08-03 00:08:26 +0700

    save

--- GameProfile/Front/angular/src/app/pages/game-pages/game/game.component.ts
+++ GameProfile/Front/angular/src/app/pages/game-pages/game/game.component.ts
@@ -1,5 +1,5 @@
-import { Component} from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 
 import { GameService } from 'src/app/services/game.service';
@@ -7,25 +7,34 @@ import { GameService } from 'src/app/services/game.service';
 
 
 @Component({
-    selector: 'game-page',
-    templateUrl: './game.component.html',
-    styleUrls: ['./game.component.css'],
-    providers: [GameService]
-  })
+  selector: 'game-page',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css'],
+  providers: [GameService]
+})
 
-  export class GameComponent{
-    id: any;
-    game: any;
-    constructor(
-      private route: ActivatedRoute,
-      private gameService: GameService
-    ){}
-  
-     ngOnInit(): void {
-      this.route.queryParams.subscribe(params => {
+export class GameComponent {
+  id: any;
+  game: any;
+  constructor(
+    private route: ActivatedRoute,
+    private gameService: GameService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
       this.id = params['id'];
-     this.gameService.fetchGame(this.id).subscribe(response => this.game = response);    
-      });
+      this.gameService.fetchGame(this.id).subscribe(response => this.game = response);
+    });
+  }
+  averageScore(): number {
+    if (!this.game?.reviews || this.game?.reviews.length === 0) {
+      return 0;
     }
+  
+    const scores = this.game?.reviews.map((review: { site: number, score: number }) => review.score);
+    const sum = scores.reduce((acc: number, score: number) => acc + score, 0);
+    return sum / scores.length;
+  }
 
-  } 
+} 
